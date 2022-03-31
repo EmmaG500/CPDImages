fd3a48e6fb96ff90c6d8198d055c9afacedc7f13
EmmaG500 <59486272+EmmaG500@users.noreply.github.com>
2022-03-31 16:37:36 +0100

    fixed syntax issues

--- uploadToBucket.py
+++ uploadToBucket.py
@@ -5,10 +5,10 @@ import boto3
 s3 = boto3.resource('s3')
 
 #string array of images to be uploaded
-imagesArr = ['/images/image1.jpg', '/images/image2.png', '/images/image3.jpg', '/images/image4.jpg', '/images/image5.jpg']
+imagesArr = ['./images/image1.jpg', './images/image2.png', './images/image3.jpg', './images/image4.jpg', './images/image5.jpg']
 #index used to help upload the right image to the bucket
 index = 1
 
 for image in imagesArr:
-  s3.meta.client.upload_file(image, 'cwbuckets2026816', ('image'+index+image[-4:])) # uploads file with a name of image, the image number, and then the image extension
-  index++
+  s3.meta.client.upload_file(image, 'cwbuckets2026816', ('image'+str(index)+image[-4:])) # uploads file with a name of image, the image number, and then the image extension
+  index+=1
