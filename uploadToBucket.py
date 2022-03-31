1f0ca6d4903a938d72880eddccfeaaf38ba724cc
EmmaG500 <59486272+EmmaG500@users.noreply.github.com>
2022-03-31 16:41:48 +0100

    Update uploadToBucket.py

--- uploadToBucket.py
+++ uploadToBucket.py
@@ -10,5 +10,6 @@ imagesArr = ['./images/image1.jpg', './images/image2.png', './images/image3.jpg'
 index = 1
 
 for image in imagesArr:
-  s3.meta.client.upload_file(image, 'cwbuckets2026816', ('image'+str(index)+image[-4:])) # uploads file with a name of image, the image number, and then the image extension
+  # copies file string from images array and uploads it to S3 Bucket with a name of image, the image number, and then the image extension
+  s3.meta.client.upload_file(image, 'cwbuckets2026816', ('image'+str(index)+image[-4:])) 
   index+=1
