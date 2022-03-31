39be0513c1614f353174b6ae34c36ca86dcc96c4
EmmaG500 <59486272+EmmaG500@users.noreply.github.com>
2022-03-31 16:28:43 +0100

    Update uploadToBucket.py

--- uploadToBucket.py
+++ uploadToBucket.py
@@ -1,38 +1,14 @@
-import os
-import sys
+#import aws sdk
 import boto3
-import time
 
-# get an access token, local (from) directory, and S3 (to) directory
-# from the command-line
-local_directory, bucket, destination = sys.argv[1:4]
+#define accessor
+s3 = boto3.resource('s3')
 
-client = boto3.client('s3')
+#string array of images to be uploaded
+imagesArr = ['/images/image1.jpg', '/images/image2.png', '/images/image3.jpg', '/images/image4.jpg', '/images/image5.jpg']
+#index used to help upload the right image to the bucket
+index = 1
 
-# enumerate local files recursively
-for root, dirs, files in os.walk(local_directory):
-
-  for filename in files:
-
-    # construct the full local path
-    local_path = os.path.join(root, filename)
-
-    # construct the full Dropbox path
-    relative_path = os.path.relpath(local_path, local_directory)
-    s3_path = os.path.join(destination, relative_path)
-
-    # relative_path = os.path.relpath(os.path.join(root, filename))
-
-    print ('Searching "%s" in "%s"' % (s3_path, bucket))
-    try:
-        client.head_object(Bucket=bucket, Key=s3_path)
-        print ("Path found on S3! Skipping %s..." % s3_path)
-
-        # try:
-            # client.delete_object(Bucket=bucket, Key=s3_path)
-        # except:
-            # print "Unable to delete %s..." % s3_path
-    except:
-        print ("Uploading %s..." % s3_path)
-        client.upload_file(local_path, bucket, s3_path)
-        time.sleep(30)
+for image in imagesArr:
+  s3.meta.client.upload_file(image, 'cwbuckets2026816', ('image'+index+image[-4:])) # uploads file with a name of image, the image number, and then the image extension
+  index++
