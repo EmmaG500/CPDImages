cb13396c34c4a1ed663c183d13a7f3d15e774c06
EmmaG500 <59486272+EmmaG500@users.noreply.github.com>
2022-04-02 12:43:06 +0100

    Update uploadToBucket.py

--- uploadToBucket.py
+++ uploadToBucket.py
@@ -8,23 +8,6 @@ import logging
 s3 = boto3.resource('s3')
 sns_client = boto3.client('sns', 'us-east-1')
 
-#string array of images to be uploaded
-imagesArr = ['./images/image1.jpg', './images/image2.png', './images/image3.jpg', './images/image4.jpg', './images/image5.jpg']
-#index used to help upload the right image to the bucket
-index = 1
-
-for image in imagesArr:
-  # copies file string from images array and uploads it to S3 Bucket with a name of image, the image number, and then the image extension
-  s3.meta.client.upload_file(image, 'thebuckets2026816', ('image'+str(index)+image[-4:])) 
-  index+=1
-  time.sleep(30) #uploads file in 30s intervals
-  publish_message('testtopics2026816', 'File Uploaded', 'str')
-  if index != 5:
-    print("File uploaded. Uploading next file...")
-  else:
-    print("All files uploaded")
-    
-  
 def publish_message(topic, message, attributes):
         """
         Publishes a message, with attributes, to a topic. Subscriptions can be filtered
@@ -54,3 +37,22 @@ def publish_message(topic, message, attributes):
             raise
         else:
             return message_id
+
+#string array of images to be uploaded
+imagesArr = ['./images/image1.jpg', './images/image2.png', './images/image3.jpg', './images/image4.jpg', './images/image5.jpg']
+#index used to help upload the right image to the bucket
+index = 1
+
+for image in imagesArr:
+  # copies file string from images array and uploads it to S3 Bucket with a name of image, the image number, and then the image extension
+  s3.meta.client.upload_file(image, 'thebuckets2026816', ('image'+str(index)+image[-4:])) 
+  index+=1
+  time.sleep(30) #uploads file in 30s intervals
+  publish_message('testtopics2026816', 'File Uploaded', 'str')
+  if index != 5:
+    print("File uploaded. Uploading next file...")
+  else:
+    print("All files uploaded")
+    
+  
+
